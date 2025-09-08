4a2bbae6e4e03224458f40af25ca3e4d832827a8
EvachiOSRS <evachitv@gmail.com>
2025-09-08 10:28:16 +0100

    Launcher edit

--- src/test/java/com/bankpricetracker/PluginLauncer.java
+++ src/test/java/com/bankpricetracker/PluginLauncer.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.bankpricetracker;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PluginLauncer
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(net.runelite.client.plugins.bankpricetracker.BankPriceTrackerPlugin.class);
 		RuneLite.main(args);
 	}
 }
